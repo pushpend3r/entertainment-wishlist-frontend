c43516171e9ec7bcf311a5e4ac99d9fd9a89ab23
Pushpender Singh <piyushthakur5678@gmail.com>
2024-05-26 15:23:11 +0530

    fixed: testing esm files with jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   testEnvironment: "jsdom",
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.tsx?$": ["ts-jest", { useESM: true }],
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
