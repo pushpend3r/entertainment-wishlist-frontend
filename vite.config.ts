2d5fee14aa9e573775edeb69927a3e3c2a40084f
Pushpender Singh <piyushthakur5678@gmail.com>
2024-05-30 23:26:08 +0530

    removed `jest` deps and added `vitest`

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import EnvironmentPlugin, { EnvVars } from "vite-plugin-environment";
@@ -7,4 +10,9 @@ const envConfig: EnvVars = ["VITE_GRAPHQL_SERVER_URI"];
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), EnvironmentPlugin(envConfig)],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.ts"],
+  },
 });
