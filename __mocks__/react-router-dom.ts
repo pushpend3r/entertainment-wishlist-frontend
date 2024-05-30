f1628d63510233363afd468656c6452d4ef775c3
Pushpender Singh <piyushthakur5678@gmail.com>
2024-05-30 23:28:13 +0530

    mocked `useIsUserLoggedIn` hook and `react-router-dom`'s useNavigate hook

---
+++ __mocks__/react-router-dom.ts
@@ -0,0 +1,5 @@
+import { vi } from "vitest";
+
+const useNavigate = vi.fn(() => vi.fn());
+
+export { useNavigate };
---
+++ src/hooks/__mocks__/useIsUserLoggedIn.ts
@@ -0,0 +1,5 @@
+import { vi } from "vitest";
+
+const mockedUseIsUserLoggedIn = vi.fn();
+
+export default mockedUseIsUserLoggedIn;
