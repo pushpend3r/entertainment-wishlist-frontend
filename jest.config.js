d77eb55e5e9fac4da3b170c9227670a51b47bfd1
Pushpender Singh <piyushthakur5678@gmail.com>
2024-05-30 23:25:15 +0530

    removed `jest` as esm mocking is getting out of hand

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
-  testEnvironment: "jsdom",
-  extensionsToTreatAsEsm: [".ts", ".tsx"],
-  transform: {
-    "^.+\\.tsx?$": ["ts-jest", { useESM: true }],
-  },
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-};
