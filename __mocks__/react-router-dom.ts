512a35b4e416376a8f6383857c9f5044c41f1835
Pushpender Singh <piyushthakur5678@gmail.com>
2024-06-01 20:49:05 +0530

    updated react-router-dom mock

--- __mocks__/react-router-dom.ts
+++ __mocks__/react-router-dom.ts
@@ -1,5 +1,8 @@
 import { vi } from "vitest";
 
 const useNavigate = vi.fn(() => vi.fn());
+const useLocation = vi.fn();
+const NavLink = vi.fn();
 
-export { useNavigate };
+export * from "react-router-dom";
+export { useNavigate, useLocation, NavLink };
