a5d915ce9f6da24fe3abd789df8ca618f6e5bb18
Pushpender Singh <piyushthakur5678@gmail.com>
2024-06-08 23:58:55 +0530

    added BE env value

--- graphql-codegen-config.ts
+++ graphql-codegen-config.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:4000",
+  schema: process.env.VITE_GRAPHQL_SERVER_URI || "http://localhost:4000",
   documents: "src/**/*.graphql.ts",
   generates: {
     "src/gql/": {
